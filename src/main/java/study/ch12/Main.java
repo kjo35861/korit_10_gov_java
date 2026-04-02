7416381d064dc3e1a48dce5387d1ecc4337a9359
kjo35861 <kkuu1044@gmail.com>
2026-04-02 17:48:44 +0900

    4.02

--- src/main/java/study/ch12/Main.java
+++ src/main/java/study/ch12/Main.java
@@ -1,5 +1,35 @@
 package study.ch12;
 
+abstract class AA {
+
+}
+
+class BB extends AA {
+
+}
+
+class CC extends BB {
+
+}
+
+class MM {
+    public static void main(String[] args) {
+        BB b = new BB();
+//        AA a = new AA(); // 추상클래스라서 생성 불가능
+        AA b2 = new BB();
+        AA c = new AA() {
+
+        };
+        AA cc = new CC();
+        AA ccc = new BB() {
+
+        };
+        BB cccc = new BB() {
+
+        };
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Dog();
